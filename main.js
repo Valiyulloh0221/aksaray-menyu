e6e8c22a6374638e61110976392fae4bb71f0433
Valiyulloh0221 <lvov11190@gmail.com>
2025-02-15 16:35:14 +0600

    dwh

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector('.scrollable-nav ul');
+const nav = document.querySelector('.category-nav ul');
 let isDown = false;
 let startX;
 let scrollLeft;
@@ -26,4 +26,23 @@ nav.addEventListener('mousemove', (e) => {
     const x = e.pageX - nav.offsetLeft;
     const walk = (x - startX) * 2; // Scroll tezligi
     nav.scrollLeft = scrollLeft - walk;
+});
+
+
+// Scroll Up Button ko‘rinishi va funksiyasi
+window.addEventListener('scroll', function () {
+    const scrollBtn = document.getElementById('scrollToTopBtn');
+    if (window.scrollY > 300) {
+        scrollBtn.style.display = 'block';
+    } else {
+        scrollBtn.style.display = 'none';
+    }
+});
+
+// Yuqoriga qaytish uchun
+document.getElementById('scrollToTopBtn').addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
